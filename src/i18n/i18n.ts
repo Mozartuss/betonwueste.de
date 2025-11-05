ea581602131a9fcb96d87580b3472e6bdc025689
Lukas Kleybolte <lukas.kleybolte@hs-augsburg.de>
2025-11-05 18:54:19 +0100

    fix translation

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,10 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-http-backend";
+import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-i18n.use(Backend)
+i18n.use(HttpBackend)
     .use(initReactI18next)
     .use(LanguageDetector)
     .init({
@@ -16,6 +16,9 @@ i18n.use(Backend)
             useSuspense: true,
             transKeepBasicHtmlNodesFor: ["br", "strong", "i", "p"],
         },
+        backend: {
+            loadPath: "/locales/{{lng}}/{{ns}}.json",
+        }
     });
 
 export default i18n;
